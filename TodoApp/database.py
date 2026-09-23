8006cc106f0ca11f536da33c22c036bf866ea202
Kaizen Ryu <natwarkumar0001@gmail.com>
2026-09-23 10:35:41 +0530

    Connected app to postgrSQL

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -3,11 +3,11 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///todosapp.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL,  connect_args={"check_same_thread": False})
+# SQLALCHEMY_DATABASE_URL = "sqlite:///todosapp.db"
+# engine = create_engine(SQLALCHEMY_DATABASE_URL,  connect_args={"check_same_thread": False})
 
-# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:ryu1234@localhost:5433/TodoApplicationDatabase'
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = 'postgresql://production_database_iwj2_user:GdmlR3O6znJSOA0lvfc8JChSsA7JoVXq@dpg-daplc0gu01pc73d25il0-a/production_database_iwj2'
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:ryu1234@127.0.0.1:3306/todoapplicationdatabase'
 # engine = create_engine(SQLALCHEMY_DATABASE_URL)
