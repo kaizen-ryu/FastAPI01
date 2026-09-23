76552a891d2feffe348d8a804b8b7dddd862bc25
Kaizen Ryu <natwarkumar0001@gmail.com>
2026-09-23 15:40:25 +0530

    changed to new production database

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 # SQLALCHEMY_DATABASE_URL = "sqlite:///todosapp.db"
 # engine = create_engine(SQLALCHEMY_DATABASE_URL,  connect_args={"check_same_thread": False})
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://production_database_iwj2_user:GdmlR3O6znJSOA0lvfc8JChSsA7JoVXq@dpg-daplc0gu01pc73d25il0-a/production_database_iwj2'
+SQLALCHEMY_DATABASE_URL = 'postgresql://prod_db_fastapi_user:TXtW5CcCQfaX2f23gsoBsdZsuIp3r5Rb@dpg-dapqa7mgekts73eqmar0-a/prod_db_fastapi'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:ryu1234@127.0.0.1:3306/todoapplicationdatabase'
